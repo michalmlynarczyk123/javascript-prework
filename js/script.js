003939bc5bb6aee2f96ea00f3fb1e649750437ca
Michał Młynarczyk <michalmlyn@gmail.com>
2021-05-23 12:14:50 +0200

    trzecia-zmiana

--- js/script.js
+++ js/script.js
@@ -20,9 +20,17 @@ playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
   playerMove = 'kamień';
-} else if (playerInput =='2') {
+} 
+else if (playerInput < '1') {
+  playerMove = 'Bład! Spróbuj jeszcze raz!';
+}
+else if (playerInput > '3') {
+  playerMove = 'Bład! Spróbuj jeszcze raz!';
+} 
+else if (playerInput == '2') {
   playerMove = 'papier';
- } else if (playerInput =='3') {
+} 
+else if (playerInput == '3') {
   playerMove = 'nożyce';
 } 
 printMessage('Twój ruch: ' + playerMove);
